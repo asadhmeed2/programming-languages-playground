f491901b838037193a4597166a97da56f8f252b5
asadhmeed2 <asadhmeed2@gmail.com>
2026-08-21 21:44:25 +0300

    feat: add BirdCount functionality to track daily bird counts in Program.cs

--- C#/basics/MyFirstApp/Program.cs
+++ C#/basics/MyFirstApp/Program.cs
@@ -9,3 +9,10 @@ checkage.start();
 var hello = new HelloWorld();
 
 Console.WriteLine(hello.Hello());
+
+int[] birdsPerDay = { 2, 5, 0, 7, 4, 1 };
+
+var birds = new  BirdCount(birdsPerDay);
+
+Console.WriteLine(birds.Today());
+
