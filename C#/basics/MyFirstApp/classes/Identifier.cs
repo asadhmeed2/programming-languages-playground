f8f9d4a29b7bbb00a8bce1c2dae80f251bb57892
asadhmeed2 <asadhmeed2@gmail.com>
2026-09-06 16:47:32 +0300

    fix: refactor Clean method to simplify control character handling and improve output formatting

--- C#/basics/MyFirstApp/classes/Identifier.cs
+++ C#/basics/MyFirstApp/classes/Identifier.cs
@@ -3,31 +3,21 @@ public static class Identifier
 {
     public static string Clean(string identifier)
     {
- 	var outputStr = "";
+        var output = "";
 
-        for(int i = 0; i < identifier.Length; i++)
-	{
-	char c = identifier[i];
-	
-	Console.WriteLine(c);
+        for(int i =0; i < identifier.Length; i++)
+        {
+            if(Char.IsControl(identifier, i))
+            {
+                output = $"{output}CTRL";
+            }
+            else
+            {
+                 output = $"{output}{identifier[i]}";
+            }
+        }
 
-	if(c == ' ')
-	{
-		outputStr = $"{outputStr}_";
-	}
-	else if(c == '\\')
-	{
-		if(i < identifier.Length && identifier[i+1] == '0')
-		{
-		i++;
-   		outputStr = $"{outputStr}CTRL";
-		}
-	}
-	else{
-		outputStr = $"{outputStr}{c}";
-	}
-	
-	}
-	return outputStr;
+        output = identifier.Replace(" ", "_");
+        return output;
     }
 }
