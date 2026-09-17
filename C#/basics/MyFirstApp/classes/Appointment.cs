1ab437025ff7a08bf7977a22b83b45668f6aff47
asadhmeed2 <asadhmeed2@gmail.com>
2026-09-17 23:23:23 +0300

    feat: implement Schedule method in Appointment class with date validation

--- C#/basics/MyFirstApp/classes/Appointment.cs
+++ C#/basics/MyFirstApp/classes/Appointment.cs
@@ -1,9 +1,29 @@
 
+using System.Globalization;
+
 static class Appointment
 {
+    static private string[] validDateFormats = { "MM/dd/yyyy hh:mm:ss", "MMM dd, yyyy hh:mm:ss", "DDD, MMM dd, yyyy hh:mm:ss" };
+    //dates format that will be used in this function are 
+    //"7/25/2019 13:45:00",
+    //"July 25, 2019 13:45:00"
+    //"Thursday, July 25, 2019 13:45:00"
     public static DateTime Schedule(string appointmentDateDescription)
     {
-        throw new NotImplementedException("Please implement the (static) Appointment.Schedule() method");
+        bool isValidDate = DateTime.TryParseExact(
+    appointmentDateDescription,
+    validDateFormats,
+    CultureInfo.InvariantCulture,
+    DateTimeStyles.None,
+    out DateTime parsedDate
+);
+        if (!isValidDate)
+        {
+            return DateTime.MinValue;
+        }
+
+        return parsedDate;
+
     }
 
     public static bool HasPassed(DateTime appointmentDate)
