a7218eb8ccd1aad9e1916221058c730292511f78
asadhmeed2 <asadhmeed2@gmail.com>
2026-09-19 17:31:22 +0300

    refactor: implement Description and AnniversaryDate methods in Appointment class

--- C#/basics/MyFirstApp/classes/Appointment.cs
+++ C#/basics/MyFirstApp/classes/Appointment.cs
@@ -37,13 +37,9 @@ static class Appointment
 
     public static bool IsAfternoonAppointment(DateTime appointmentDate)=> appointmentDate.Hour >= 12 && appointmentDate.Hour < 18;
 
-    public static string Description(DateTime appointmentDate)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.Description() method");
-    }
+    public static string Description(DateTime appointmentDate) => $"You have an appointment on {appointmentDate.ToString()}.";
+    
+    
 
-    public static DateTime AnniversaryDate()
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.AnniversaryDate() method");
-    }
+    public static DateTime AnniversaryDate()=> new DateTime(DateTime.Now.Year, 9, 15, 0, 0, 0);
 }
